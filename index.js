bac65c35416a07ca1c756cab7245948373d8584b
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2021-12-27 19:59:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -92,3 +92,68 @@
 // console.log(Number.isNaN(NaN));
 
 //part-8: expressions & operators
+
+//part-8.1:Assignment Operator is ( = )
+
+//part-8.2:Arithmetic Operators
+// console.log(3+3);
+// console.log(10-3);
+// console.log(20/5);
+// console.log(5*6);
+// console.log("Remainder Operator " + 81%8);
+
+//part-8.2.1:Increment & decrement ++x or x++ & --x or x--
+
+//part-8.3: Comparison operators
+
+// var a = 30;
+// var b = 20;
+//part-8.3.1: Equel (==)
+//console.log(a==b);
+//part-8.3.2: Not Equel (!=)
+//console.log(a!=b);
+//part-8.3.3: Greater than (>)
+//console.log(a>b);
+//part-8.3.4: Greater than or equal (>=)
+//console.log(a>=b);
+//part-8.3.5: Less than (<)
+//console.log(a<b);
+//part-8.3.6: Less than Equal (<=)
+//console.log(a<=b);
+
+
+//part-8.4: Logical operators
+
+//part-8.4.1: Logical AND ( && )
+//console.log(a<b && a>0);
+//part-8.4.2: Logical OR ( || )
+//console.log(a<b || a>0);
+//part-8.4.1: Logical NOT ( ! )
+//console.log(!((a<b) && (a>0)));
+//console.log(!false);
+
+//part-8.5: String Concatenation (operators is ( + ))
+//console.log("Hello world");
+//console.log("Hello " + "world"); // Concatenation
+
+//part-9: challange-3: power declare (3**3)
+// //part-10: challange-4: swap two variable value
+//  var a = 5;
+//  var b = 10;
+//   c = a;
+//   a = b;
+//   b = c;
+//  console.log( a);
+//  console.log( b);
+
+// //part-11: challange-5: swap two variable value with out temp variable
+// var a = 5;
+// var b = 10;
+//  a = a+b;
+//  b = a-b;
+//  a = a-b;
+// console.log( a);
+// console.log( b);
+//part-12: Interview Question-3 : diff between == vs === ?
+//(==) is check only values
+//(===) is check value and datatype
