31f574ac2c2177f6284ae053af45fce1b6895e71
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-02-07 19:55:43 +0600

    Create Event.js

---
+++ Event/Event.js
@@ -0,0 +1,25 @@
+//section 11: EVENTS in JavaScript
+
+//HTML events are "things" that happen to html elements.
+//When JavaScript is used in HTML pages, JAvaScript can "react" on these events.
+
+// ➡️ HTML Events:
+// An HTML event can be something the browser does, or something a user does.
+// Here are some examples of HTML events:
+
+// 1) An HTML web page has finished loading
+// 2) An HTML input field was changed
+// 3) An HTML button was clicked
+// 4) often, when events happen, you may want to do something.
+
+//JavaScript lets you execute code when events are detected.
+//HTML allows event handler attributes, with JavaScript code, to be added to HTML elements.
+
+// section 1️⃣ 4 ways of writing Events in JavaScript.
+
+// 1: using inline events alert();
+// 2: By calling a function (We already seen and most common way of writting)
+// 3: using Inline Events (HTML onclick=" " property and element.onclick)
+// 4: using Event Listeners (addEventListener and IE's attachEvent)
+
+//check the Events HTML file
